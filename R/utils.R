eb0589582ac534b507457690d60fbe42e6be5351
Athos Damiani <athos.damiani@gmail.com>
2021-03-09 18:00:52 -0300

    set progress bar func

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,15 @@
 clean_string <- function(str) {
   gsub("\\W", "_", str)
 }
+
+#' Set progress bar
+#'
+#' @param total (int) total duration of the progress bar.
+#'
+#' @export
+set_progress_bar <- function(total) {
+  progress::progress_bar$new(
+    total = total, clear = FALSE, width = 70,
+    format = ":current/:total [:bar] - :elapsed - loss: :loss"
+  )
+}
